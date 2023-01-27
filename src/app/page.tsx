1109d99e31d6fc8bb737c7c80ba479bee050afc8
Raymond Su <tricatawing@gmail.com>
2023-01-27 11:27:34 +0800

    fourth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,31 +19,31 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            {/*<Image*/}
+            {/*  src="/vercel.svg"*/}
+            {/*  alt="Vercel Logo"*/}
+            {/*  className={styles.vercelLogo}*/}
+            {/*  width={100}*/}
+            {/*  height={24}*/}
+            {/*  priority*/}
+            {/*/>*/}
           </a>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+      {/*<div className={styles.center}>*/}
+      {/*  <Image*/}
+      {/*    className={styles.logo}*/}
+      {/*    src="/next.svg"*/}
+      {/*    alt="Next.js Logo"*/}
+      {/*    width={180}*/}
+      {/*    height={37}*/}
+      {/*    priority*/}
+      {/*  />*/}
+      {/*  <div className={styles.thirteen}>*/}
+      {/*    <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />*/}
+      {/*  </div>*/}
+      {/*</div>*/}
 
       <div className={styles.grid}>
         <a
